8056f69862e891c4eb5451e46e882ce7a5559821
Vitor Hugo <vitorhugoss17@gmail.com>
2025-12-04 09:14:06 -0300

    remove treesiter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,47 +1,47 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects", -- Opcional mas muito útil
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "clojure",
-        "python",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "json",
-        "java",
-        "markdown",
-        "markdown_inline",
-      },
-
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true,
-      },
-
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-      },
-    })
-  end,
-}
+--return {
+--  "nvim-treesitter/nvim-treesitter",
+--  build = ":TSUpdate",
+--  event = { "BufReadPost", "BufNewFile" },
+--  dependencies = {
+--    "nvim-treesitter/nvim-treesitter-textobjects", -- Opcional mas muito útil
+--  },
+--  config = function()
+--    require("nvim-treesitter.configs").setup({
+--      ensure_installed = {
+--        "lua",
+--        "vim",
+--        "vimdoc",
+--        "clojure",
+--        "python",
+--        "javascript",
+--        "typescript",
+--        "html",
+--        "css",
+--        "json",
+--        "java",
+--        "markdown",
+--        "markdown_inline",
+--      },
+--
+--      auto_install = true,
+--
+--      highlight = {
+--        enable = true,
+--        additional_vim_regex_highlighting = true,
+--      },
+--
+--      textobjects = {
+--        select = {
+--          enable = true,
+--          lookahead = true,
+--          keymaps = {
+--            ["af"] = "@function.outer",
+--            ["if"] = "@function.inner",
+--            ["ac"] = "@class.outer",
+--            ["ic"] = "@class.inner",
+--          },
+--        },
+--      },
+--    })
+--  end,
+--}
