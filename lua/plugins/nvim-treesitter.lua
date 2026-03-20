c08b83fbedb1a26776748c78b840b73f5f91fbad
Vitor Hugo <vitorhugoss17@gmail.com>
2026-03-20 18:15:52 -0300

    backup windows

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -7,24 +7,42 @@ return {
     "nvim-treesitter/nvim-treesitter-textobjects", -- Opcional mas muito útil
   },
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "clojure",
-        "python",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "json",
-        "java",
-        "markdown",
-        "markdown_inline",
-      },
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    local is_windows = vim.loop.os_uname().version:find("Windows") or vim.fn.has("win32") == 1
+    local parsers_dir = vim.fn.stdpath("data") .. "/treesitter-parsers"
 
-      auto_install = true,
+    local langs = {
+      "lua",
+      "vim",
+      "vimdoc",
+      "clojure",
+      "python",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "json",
+      "java",
+      "markdown",
+      "markdown_inline",
+    }
+
+    -- Se estiver no Windows e a pasta local de parsers existir, aponta as URLs para lá
+    if is_windows then
+      for _, lang in ipairs(langs) do
+        if parser_config[lang] then
+          parser_config[lang].install_info.url = parsers_dir .. "/" .. lang
+        end
+      end
+    end
+
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = langs,
+      auto_install = not is_windows, -- No Windows, faremos manual via local
+
+      install = {
+        compilers = is_windows and { "clang" } or nil,
+      },
 
       highlight = {
         enable = true,
